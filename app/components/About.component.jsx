841ac7e0855e411d4c7b968a991b61b498d95390
marinm <mestrovic.ma@gmail.com>
2017-02-11 12:41:55 -0500

    fix error modal bug

--- app/components/About.component.jsx
+++ app/components/About.component.jsx
@@ -13,7 +13,6 @@ const About = (props) => {
           <a href="http://openweathermap.org">Open Weather Map</a> -I used
           Open Weather Map to search for weather data by city name.
         </li>
-        <li></li>
       </ul>
     </div>
   );
--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import ReactDOMServer from "react-dom/server";
+
 
 export class ErrorModal extends React.Component {
   componentDidMount() {
-    const modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  }
-
-  render() {
-    return (
+    const modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{this.props.title}</h4>
         <p>{this.props.message}</p>
@@ -17,6 +15,17 @@ export class ErrorModal extends React.Component {
           </button>
         </div>
       </div>
+    );
+    const $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    const modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  }
+
+  render() {
+    return (
+      <div></div>
     );
   }
 }
